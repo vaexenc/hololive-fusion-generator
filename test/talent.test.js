8dcab3c021c3d014bbbb65e8a09a092c5a03615c
vaexenc <53697802+vaexenc@users.noreply.github.com>
2021-03-20 15:43:37 +0100

    add tests for getTalentByID() and all newly created functions

--- test/talent.test.js
+++ test/talent.test.js
@@ -1,4 +1,5 @@
 const talent = require("../src/talent");
+const util = require("../src/util");
 
 describe("getTalentByID()", () => {
 	it("always returns the same object when called with the same id", () => {
@@ -34,5 +35,63 @@ describe("getTalentByID()", () => {
 		const t = talent.getTalentByID("pekora");
 		expect(t.lastName).toEqual("usada");
 		expect(t.firstName).toEqual("pekora");
+	});
+
+	it("doesn't return an array", () => {
+		const t = talent.getTalentByID("pekora");
+		expect(Array.isArray(t)).toBe(false);
+	});
+
+	it("returns an object", () => {
+		const t = talent.getTalentByID("pekora");
+		expect(typeof t).toBe("object");
+	});
+});
+
+describe("getAmountOfTalents()", () => {
+	const amountOfTalents = talent.getAmountOfTalents();
+
+	it("should be greater than 40", () => {
+		expect(amountOfTalents).toBeGreaterThan(40);
+	});
+
+	it("should be less than 100", () => {
+		expect(amountOfTalents).toBeLessThan(100);
+	});
+});
+
+describe("getIDsOfEnabledTalents()", () => {
+	const enabledTalentIDs = talent.getIDsOfEnabledTalents();
+
+	it("returns an array", () => {
+		expect(Array.isArray(enabledTalentIDs)).toBe(true);
+	});
+
+	it("has a length equal to getAmountOfEnabledTalents()", () => {
+		expect(enabledTalentIDs.length).toBe(talent.getAmountOfEnabledTalents());
+	});
+});
+
+describe("getAmountOfEnabledTalents()", () => {
+	const amountOfEnabledTalents = talent.getAmountOfEnabledTalents();
+	const amountOfTalents = talent.getAmountOfTalents();
+
+	it("should be greater than or equal to 0", () => {
+		expect(amountOfEnabledTalents).toBeGreaterThanOrEqual(0);
+	});
+
+	it("should probably be greater than 0", () => {
+		expect(amountOfEnabledTalents).toBeGreaterThan(0);
+	});
+
+	it("should be less than or equal to total amount of talents", () => {
+		expect(amountOfEnabledTalents).toBeLessThanOrEqual(amountOfTalents);
+	});
+});
+
+describe("getAmountOfTalentVariations()", () => {
+	it("returns the same thing as when calculating manually", () => {
+		expect(talent.getAmountOfTalentVariations())
+		.toBe(util.getAmountOfVariations(talent.getAmountOfEnabledTalents(), 2));
 	});
 });
