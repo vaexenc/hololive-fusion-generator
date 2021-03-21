34399d9f0add2ae2b6664ae853483d4fabe06a3d
vaexenc <53697802+vaexenc@users.noreply.github.com>
2021-03-21 21:01:05 +0100

    move fuseTalentFullNamesById() test to top of file

--- test/name.test.js
+++ test/name.test.js
@@ -1,6 +1,63 @@
 const name = require("../src/name");
 const talent = require("../src/talent");
 
+describe("fuseTalentFullNamesById()", () => {
+	it("returns name unchanged when both talents are the same", () => {
+		const fullName = name.fuseTalentFullNamesById("gura", "gura");
+		expect(fullName).toEqual({lastName: "Gawr", firstName: "Gura"});
+	});
+
+	it("creates 'Shishimaki Botame' from botan and watame", () => {
+		const fullName = name.fuseTalentFullNamesById("botan", "watame");
+		expect(fullName).toEqual({lastName: "Shishimaki", firstName: "Botame"});
+	});
+
+	it("creates 'Tsunoshiro Watatan' from watame and botan", () => {
+		const fullName = name.fuseTalentFullNamesById("watame", "botan");
+		expect(fullName).toEqual({lastName: "Tsunoshiro", firstName: "Watatan"});
+	});
+
+	it("creates 'Takamori Killiope' from kiara and calli", () => {
+		const fullName = name.fuseTalentFullNamesById("kiara", "calli");
+		expect(fullName).toEqual({lastName: "Takamori", firstName: "Killiope"});
+	});
+
+	it("creates 'Morinashi Calliara' from calli and kiara", () => {
+		const fullName = name.fuseTalentFullNamesById("calli", "kiara");
+		expect(fullName).toEqual({lastName: "Morinashi", firstName: "Calliara"});
+	});
+
+	it("creates 'Haachagoo' from haachama and yagoo (firstName)", () => {
+		const fullName = name.fuseTalentFullNamesById("haachama", "yagoo");
+		expect(fullName).toEqual({lastName: undefined, firstName: "Haachagoo"});
+	});
+
+	it("creates 'Yachama' from yagoo and haachama (firstName)", () => {
+		const fullName = name.fuseTalentFullNamesById("yagoo", "haachama");
+		expect(fullName).toEqual({lastName: undefined, firstName: "Yachama"});
+	});
+
+	it("creates 'AZboco' from azki and roboco (firstName)", () => {
+		const fullName = name.fuseTalentFullNamesById("azki", "roboco");
+		expect(fullName).toEqual({lastName: undefined, firstName: "AZboco"});
+	});
+
+	it("creates 'RoboKi' from roboco and azki (firstName)", () => {
+		const fullName = name.fuseTalentFullNamesById("roboco", "azki");
+		expect(fullName).toEqual({lastName: undefined, firstName: "RoboKi"});
+	});
+
+	it("creates 'Watson Amechama' from ame and haachama", () => {
+		const fullName = name.fuseTalentFullNamesById("ame", "haachama");
+		expect(fullName).toEqual({lastName: "Watson", firstName: "Amechama"});
+	});
+
+	it("creates 'Watson Haachamelia' from haachama and ame", () => {
+		const fullName = name.fuseTalentFullNamesById("haachama", "ame");
+		expect(fullName).toEqual({lastName: "Watson", firstName: "Haachamelia"});
+	});
+});
+
 describe("fuseNameChunks()", () => {
 	it("creates 'Takamori' from 'taka' and 'mori'", () => {
 		expect(name.fuseNameChunks("taka", "mori")).toBe("Takamori");
@@ -72,62 +129,5 @@ describe("printAllNameVariations()", () => {
 		name.printAllNameVariations("gura");
 		expect(logSpy).toHaveBeenCalledTimes(talent.getAmountOfTotalTalentVariations());
 		logSpy.mockRestore();
-	});
-});
-
-describe("fuseTalentFullNamesById()", () => {
-	it("returns name unchanged when both talents are the same", () => {
-		const fullName = name.fuseTalentFullNamesById("gura", "gura");
-		expect(fullName).toEqual({lastName: "Gawr", firstName: "Gura"});
-	});
-
-	it("creates 'Shishimaki Botame' from botan and watame", () => {
-		const fullName = name.fuseTalentFullNamesById("botan", "watame");
-		expect(fullName).toEqual({lastName: "Shishimaki", firstName: "Botame"});
-	});
-
-	it("creates 'Tsunoshiro Watatan' from watame and botan", () => {
-		const fullName = name.fuseTalentFullNamesById("watame", "botan");
-		expect(fullName).toEqual({lastName: "Tsunoshiro", firstName: "Watatan"});
-	});
-
-	it("creates 'Takamori Killiope' from kiara and calli", () => {
-		const fullName = name.fuseTalentFullNamesById("kiara", "calli");
-		expect(fullName).toEqual({lastName: "Takamori", firstName: "Killiope"});
-	});
-
-	it("creates 'Morinashi Calliara' from calli and kiara", () => {
-		const fullName = name.fuseTalentFullNamesById("calli", "kiara");
-		expect(fullName).toEqual({lastName: "Morinashi", firstName: "Calliara"});
-	});
-
-	it("creates 'Haachagoo' from haachama and yagoo (firstName)", () => {
-		const fullName = name.fuseTalentFullNamesById("haachama", "yagoo");
-		expect(fullName).toEqual({lastName: undefined, firstName: "Haachagoo"});
-	});
-
-	it("creates 'Yachama' from yagoo and haachama (firstName)", () => {
-		const fullName = name.fuseTalentFullNamesById("yagoo", "haachama");
-		expect(fullName).toEqual({lastName: undefined, firstName: "Yachama"});
-	});
-
-	it("creates 'AZboco' from azki and roboco (firstName)", () => {
-		const fullName = name.fuseTalentFullNamesById("azki", "roboco");
-		expect(fullName).toEqual({lastName: undefined, firstName: "AZboco"});
-	});
-
-	it("creates 'RoboKi' from roboco and azki (firstName)", () => {
-		const fullName = name.fuseTalentFullNamesById("roboco", "azki");
-		expect(fullName).toEqual({lastName: undefined, firstName: "RoboKi"});
-	});
-
-	it("creates 'Watson Amechama' from ame and haachama", () => {
-		const fullName = name.fuseTalentFullNamesById("ame", "haachama");
-		expect(fullName).toEqual({lastName: "Watson", firstName: "Amechama"});
-	});
-
-	it("creates 'Watson Haachamelia' from haachama and ame", () => {
-		const fullName = name.fuseTalentFullNamesById("haachama", "ame");
-		expect(fullName).toEqual({lastName: "Watson", firstName: "Haachamelia"});
 	});
 });
