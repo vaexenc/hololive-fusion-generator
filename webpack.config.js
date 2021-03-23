32a6293f781e978d4399f8d23301a2556aa0f51c
vaexenc <53697802+vaexenc@users.noreply.github.com>
2021-03-23 20:58:14 +0100

    create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+	entry: {
+		main: "./src/main.mjs"
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "./bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, "css-loader"]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({template: "./src/index.html"}),
+		new MiniCssExtractPlugin({filename: "style.css"})
+	]
+};
