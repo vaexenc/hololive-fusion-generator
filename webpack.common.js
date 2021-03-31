d26c259d72ab8a72f81076e773efc0f88743684a
vaexenc <53697802+vaexenc@users.noreply.github.com>
2021-03-31 13:24:56 +0200

    add url: false to css-loader options

--- webpack.common.js
+++ webpack.common.js
@@ -15,7 +15,15 @@ module.exports = {
 		rules: [
 			{
 				test: /\.css$/,
-				use: [MiniCssExtractPlugin.loader, "css-loader"]
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+						options: {
+							url: false
+						}
+					}
+				]
 			}
 		]
 	},
