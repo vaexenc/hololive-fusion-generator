c01838c995cf259bec6e885cefa92356098a1e63
vaexenc <53697802+vaexenc@users.noreply.github.com>
2021-04-03 22:13:51 +0200

    commit first frontend WIP

--- src/main.mjs
+++ src/main.mjs
@@ -1,1 +1,16 @@
 import "./style.css";
+
+const elemMain = document.querySelector(".main");
+const elemResult = document.querySelector(".result-box");
+
+function createAndInsertTalentSelectElements() {
+	const template = document.querySelector(".template-talent-select-box");
+	const firstClone = template.content.cloneNode(true);
+	const secondClone = template.content.cloneNode(true);
+	firstClone.querySelector(".talent-select-box").classList.add("talent-select-box-1");
+	secondClone.querySelector(".talent-select-box").classList.add("talent-select-box-2");
+	elemMain.insertBefore(firstClone, elemResult);
+	elemMain.appendChild(secondClone);
+}
+
+createAndInsertTalentSelectElements();
