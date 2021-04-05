1a6526c6d49d923c7486ba860044d85fde914ce0
vaexenc <53697802+vaexenc@users.noreply.github.com>
2021-04-05 20:45:24 +0200

    implement background cycling and saving

--- src/main.mjs
+++ src/main.mjs
@@ -1,10 +1,12 @@
 import "./style.css";
 import {backgroundImages} from "./backgroundImages.js";
-import {getRandomInt} from "./util.js";
+import {getRandomInt, mod} from "./util.js";
 
 const elemMain = document.querySelector(".main");
 const elemResult = document.querySelector(".result-box");
 const elemBackground = document.querySelector(".background");
+
+let backgroundIndex;
 
 function createAndInsertTalentSelectContainers() {
 	const template = document.querySelector(".template-talent-select-box");
@@ -16,21 +18,54 @@ function createAndInsertTalentSelectContainers() {
 	elemMain.appendChild(secondClone);
 }
 
-function getBackgroundCurrent() {
-	return elemBackground.style.backgroundImage;
-}
-
-function setBackground(filename) {
-	elemBackground.style.backgroundImage = `url(images/backgrounds/${filename})`;
+function setBackground(index) {
+	backgroundIndex = index;
+	elemBackground.style.backgroundImage = `url(images/backgrounds/${backgroundImages[index]})`;
 }
 
 function setBackgroundRandom() {
-	setBackground(backgroundImages[getRandomInt(backgroundImages.length)]);
+	setBackground(getRandomInt(backgroundImages.length));
+}
+
+function saveCurrentBackgroundIndex() {
+	localStorage.setItem("backgroundIndex", backgroundIndex);
+}
+
+function loadSavedBackgroundIndex() {
+	return localStorage.getItem("backgroundIndex");
+}
+
+function deleteSavedBackgroundIndex() {
+	localStorage.removeItem("backgroundIndex");
+}
+
+function keepBackgroundIndexWithinBounds(index) {
+	return mod(index, backgroundImages.length);
+}
+
+function cycleBackgroundForwards() {
+	backgroundIndex = keepBackgroundIndexWithinBounds(++backgroundIndex);
+	setBackground(backgroundIndex);
+	saveCurrentBackgroundIndex(backgroundIndex);
+}
+
+function cycleBackgroundBackwards() {
+	backgroundIndex = keepBackgroundIndexWithinBounds(--backgroundIndex);
+	setBackground(backgroundIndex);
+	saveCurrentBackgroundIndex(backgroundIndex);
+}
+
+function decideBackground() {
+	const saved = loadSavedBackgroundIndex();
+	if (saved)
+		setBackground(saved);
+	else
+		setBackgroundRandom();
 }
 
 function main() {
 	createAndInsertTalentSelectContainers();
-	setBackgroundRandom();
+	decideBackground();
 }
 
 main();
