3e04a200a29adcaffc8012929a13628513bdfd42
vaexenc <53697802+vaexenc@users.noreply.github.com>
2021-04-05 20:16:07 +0200

    add tests for mod()

--- test/util.test.js
+++ test/util.test.js
@@ -63,3 +63,37 @@ describe("getRandomInt()", () => {
 			expect(util.getRandomInt(5)).toBeLessThan(5);
 	});
 });
+
+describe("mod()", () => {
+	it("returns 0 when mod(0, 10)", () => {
+		expect(util.mod(0, 10)).toBe(0);
+	});
+
+	it("returns 5 when mod(5, 10)", () => {
+		expect(util.mod(5, 10)).toBe(5);
+	});
+
+	it("returns 0 when mod(10, 10)", () => {
+		expect(util.mod(10, 10)).toBe(0);
+	});
+
+	it("returns 0 when mod(30, 10)", () => {
+		expect(util.mod(30, 10)).toBe(0);
+	});
+
+	it("returns 9 when mod(-1, 10)", () => {
+		expect(util.mod(-1, 10)).toBe(9);
+	});
+
+	it("returns 1 when mod(-9, 10)", () => {
+		expect(util.mod(-9, 10)).toBe(1);
+	});
+
+	it("returns 0 when mod(-20, 10)", () => {
+		expect(util.mod(-20, 10)).toBe(0);
+	});
+
+	it("returns 6 when mod(-34, 10)", () => {
+		expect(util.mod(-34, 10)).toBe(6);
+	});
+});
