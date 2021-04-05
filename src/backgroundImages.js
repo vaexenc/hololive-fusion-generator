11545e82237ceae3486cf7a9cf4728cfa690d82f
vaexenc <53697802+vaexenc@users.noreply.github.com>
2021-04-05 02:16:20 +0200

    create backgroundImages.js

---
+++ src/backgroundImages.js
@@ -0,0 +1,24 @@
+// val-loader executable
+
+const fs = require("fs");
+
+function getBgFileNumber(fileName) {
+	return parseInt(fileName.match(/^bg(\d+)\.(jpg|png)/)[1]);
+}
+
+function bgFilenameSort(a, b) {
+	if (getBgFileNumber(a) < getBgFileNumber(b))
+		return -1;
+	return 1;
+}
+
+const bgFilenames = fs.readdirSync("./dist/images/backgrounds");
+const bgFilenamesSorted = bgFilenames.sort(bgFilenameSort);
+
+module.exports = () => {
+	return {
+		code: `module.exports = {
+			backgroundImages: ${JSON.stringify(bgFilenamesSorted)}
+		}`
+	};
+};
