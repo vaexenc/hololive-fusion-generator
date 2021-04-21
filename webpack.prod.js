a1467e9f4bff7c751775d5d2ae49fdcc6ecd58a2
vaexenc <53697802+vaexenc@users.noreply.github.com>
2021-04-21 16:39:02 +0200

    use css-minimizer-webpack-plugin and terser-webpack-plugin (default)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,15 @@
 const common = require("./webpack.common.js");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const {merge} = require("webpack-merge");
 
 module.exports = merge(common, {
-	mode: "production"
+	mode: "production",
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin(),
+			new CssMinimizerPlugin()
+		]
+	}
 });
