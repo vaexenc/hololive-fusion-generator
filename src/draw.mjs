de59a4590abdc8935268dd9aef4ae9b959416474
vaexenc <53697802+vaexenc@users.noreply.github.com>
2021-05-30 17:03:32 +0200

    implement drawing of hair, body, etc palettes

--- src/draw.mjs
+++ src/draw.mjs
@@ -1,11 +1,232 @@
-function drawFusion(canvas, id1, id2) {
-	const ctx = canvas.getContext("2d");
+import {getTalentById} from "./talent.js";
+import {mod} from "./util.js";
+
+const path = "images/talents/";
+const ext = ".webp";
+
+const paletteTypes = ["hair", "body", "etc"];
+
+function createImage(url) {
+	const image = new Image();
+	image.src = url;
+	return image;
+}
+
+function createImageLoadPromise(image) {
+	return new Promise((resolve) => {
+		image.onload = () => resolve(image);
+	});
+}
+
+function addImageToImageLoadPromises(image, imageLoadPromises) {
+	imageLoadPromises.push(createImageLoadPromise(image));
+}
+
+async function createManifest(id1, id2) {
+	const talent1 = getTalentById(id1);
+	const talent2 = getTalentById(id2);
+	const manifest = {};
+	const imageLoadPromises = [];
+
+	const baseImage = createImage(path + id2 + "-base" + ext);
+	manifest.baseImage = baseImage;
+	addImageToImageLoadPromises(baseImage, imageLoadPromises);
+
+	for (const paletteType of paletteTypes) {
+		if (!talent2.drawSecond[paletteType])
+			continue;
+
+		manifest[paletteType] = {};
+
+		manifest[paletteType].colors = talent1.drawFirst[paletteType].colors;
+
+		manifest[paletteType].images = [];
+		for (let i = 0; i < talent2.drawSecond[paletteType].count; i++) {
+			const image = createImage(path + id2 + "-" + paletteType + "-" + i + ext);
+			manifest[paletteType].images.push(image);
+			addImageToImageLoadPromises(image, imageLoadPromises);
+		}
+	}
+
+	await Promise.all(imageLoadPromises);
+	return manifest;
+}
+
+function assertImagesAreSameSize(images) {
+	if (images.length <= 1) return;
+	const {width, height} = images[0];
+	for (const image of images) {
+		if (image.width !== width || image.height !== height)
+			throw new Error("images are of different sizes");
+	}
+}
+
+function lerp(v0, v1, t) {
+	return v0 * (1-t) + v1 * t;
+}
+
+function getAspectRatio(width, height) {
+	return width / height;
+}
+
+// function isWidthGreaterThanHeight(width, height) {
+// 	return getAspectRatio(width, height) > 1;
+// }
+
+// function isHeightGreaterThanWidth(width, height) {
+// 	return !isWidthGreaterThanHeight(width, height);
+// }
+
+function proportionalScaleWidth(widthNew, widthOld, heightOld) {
+	const factor = widthNew / widthOld;
+	return {
+		width: widthNew,
+		height: heightOld * factor
+	};
+}
+
+function proportionalScaleHeight(heightNew, widthOld, heightOld) {
+	const factor = heightNew / heightOld;
+	return {
+		width: widthOld * factor,
+		height: heightNew
+	};
+}
+
+function isAspectRatioGreaterThanOther(width1, height1, width2, height2) {
+	if (getAspectRatio(width1, height1) > getAspectRatio(width2, height2))
+		return true;
+	return false;
+}
+
+function resizeToContain(widthTarget, heightTarget, width, height) {
+	if (isAspectRatioGreaterThanOther(width, height, widthTarget, heightTarget))
+		return proportionalScaleWidth(widthTarget, width, height);
+	return proportionalScaleHeight(heightTarget, width, height);
+}
+
+function resizeToCover(widthTarget, heightTarget, width, height) {
+	if (isAspectRatioGreaterThanOther(width, height, widthTarget, heightTarget))
+		return proportionalScaleHeight(heightTarget, width, height);
+	return proportionalScaleWidth(widthTarget, width, height);
+}
+
+function getAlignPos(lengthTarget, length, t) {
+	return lerp(0, lengthTarget - length, t);
+}
+
+function clearCanvas(ctx) {
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+function drawBg(ctx) {
+	ctx.save();
 	ctx.fillStyle = "white";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+	ctx.restore();
+}
+
+function drawDebugNames(ctx, id1, id2) {
+	ctx.save();
 	ctx.fillStyle = "black";
-	ctx.font = `${Math.min(canvas.width*0.08, canvas.height*0.08)}px Arial`;
-	ctx.textAlign = "center";
-	ctx.fillText(id1 + " " + id2, canvas.width/2, canvas.height/2);
+	ctx.font = `${Math.min(ctx.canvas.width*0.08, ctx.canvas.height*0.08)}px Arial`;
+	ctx.textAlign = "left";
+	ctx.fillText(id1 + " " + id2, ctx.canvas.width*0.03, ctx.canvas.height*0.09);
+	ctx.restore();
+}
+
+// function copyCanvasOntoAnother(canvas1, canvas2) {
+// 	const ctx = canvas2.getContext("2d");
+// 	ctx.drawImage(canvas1, 0, 0, canvas1.width, canvas2.height);
+// }
+
+function createCanvasSameSize(canvas) {
+	const newCanvas = document.createElement("canvas");
+	newCanvas.width = canvas.width;
+	newCanvas.height = canvas.height;
+	return newCanvas;
+}
+
+async function drawResultCanvas(id1, id2) {
+	const canvas = document.createElement("canvas");
+	const ctx = canvas.getContext("2d");
+
+	if (id1 === id2) {
+		const originalImage = createImage(path + id1 + "-original" + ext);
+		await createImageLoadPromise(originalImage);
+		canvas.width = originalImage.width;
+		canvas.height = originalImage.height;
+		ctx.drawImage(originalImage, 0, 0);
+		return canvas;
+	}
+
+	const manifest = await createManifest(id1, id2);
+
+	const paletteImages = [];
+	for (const paletteType of paletteTypes) {
+		if (manifest[paletteType]) {
+			for (const image of manifest[paletteType].images) {
+				paletteImages.push(image);
+			}
+		}
+	}
+
+	assertImagesAreSameSize(paletteImages.concat([manifest.baseImage]));
+
+	canvas.width = manifest.baseImage.width;
+	canvas.height = manifest.baseImage.height;
+
+	const baseImageBlackAndWhite = createCanvasSameSize(canvas);
+	const ctxbw = baseImageBlackAndWhite.getContext("2d");
+	ctxbw.drawImage(manifest.baseImage, 0, 0);
+	ctxbw.globalCompositeOperation = "saturation";
+	ctxbw.fillStyle = "black";
+	ctxbw.fillRect(0, 0, baseImageBlackAndWhite.width, baseImageBlackAndWhite.height);
+
+	const rawPalettes = createCanvasSameSize(canvas);
+	const ctxp = rawPalettes.getContext("2d");
+	for (const paletteImage of paletteImages) {
+		ctxp.drawImage(paletteImage, 0, 0);
+	}
+
+	ctxbw.globalCompositeOperation = "destination-in";
+	ctxbw.drawImage(rawPalettes, 0, 0);
+
+	ctx.drawImage(manifest.baseImage, 0, 0);
+	ctx.drawImage(baseImageBlackAndWhite, 0, 0);
+
+	for (const paletteType of paletteTypes) {
+		if (!manifest[paletteType]) continue;
+		for (const [i, paletteImage] of manifest[paletteType].images.entries()) {
+			ctx.save();
+			const color = manifest[paletteType].colors[mod(i, manifest[paletteType].colors.length)];
+			const paletteLayer = createCanvasSameSize(canvas);
+			const ctxl = paletteLayer.getContext("2d");
+			ctxl.drawImage(paletteImage, 0, 0);
+			ctxl.fillStyle = color;
+			ctxl.globalCompositeOperation = "overlay";
+			ctxl.fillRect(0, 0, paletteLayer.width, paletteLayer.height);
+			ctxl.globalCompositeOperation = "destination-in";
+			ctxl.drawImage(paletteImage, 0, 0);
+			ctx.drawImage(paletteLayer, 0, 0);
+			ctx.restore();
+		}
+	}
+
+	return canvas;
+}
+
+let currentDrawId = 0;
+
+async function drawFusion(canvasDOM, id1, id2) {
+	const drawId = ++currentDrawId;
+	const ctx = canvasDOM.getContext("2d");
+	clearCanvas(ctx);
+	const canvasResult = await drawResultCanvas(id1, id2);
+	if (currentDrawId !== drawId) return;
+	drawBg(ctx);
+	const {width: w, height: h} = resizeToContain(canvasDOM.width, canvasDOM.height, canvasResult.width, canvasResult.height);
+	ctx.drawImage(canvasResult, getAlignPos(canvasDOM.width, w, 0.5), getAlignPos(canvasDOM.height, h, 1), w, h);
 }
 
 export {drawFusion};
